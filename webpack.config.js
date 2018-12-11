410b85d6d7ea22c46bfd3feb33fac0bb5fe88270
Junho Baik <junhobaik@gmail.com>
2018-12-11 13:31:05 +0900

    Modify webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,38 @@
-const ManifestPlugin = require("webpack-manifest-plugin");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ManifestPlugin = require('webpack-manifest-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
 module.exports = {
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "index.html"
+      template: './public/index.html',
+      filename: 'index.html',
     }),
     new ManifestPlugin({
-      fileName: "manifest.json",
-      basePath: "./dist/"
-    })
-  ]
+      fileName: 'manifestWebpack.json',
+      basePath: './dist/',
+    }),
+    new CopyWebpackPlugin([{ from: 'public', to: '' }]),
+  ],
 };
