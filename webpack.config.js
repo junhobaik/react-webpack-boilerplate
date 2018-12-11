b23859216aaf2bdaa94d804ad1bdeaa77edd5851
Junho Baik <junhobaik@gmail.com>
2018-12-11 12:36:42 +0900

    webpack config, add scss configs

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,8 @@ module.exports = {
         }
       },
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
       }
     ]
   },
